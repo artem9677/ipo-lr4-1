a60d746247cb5ad99ad31efc1af34f5c92f3e206
Artem <69763892+artem9677@users.noreply.github.com>
2024-10-20 16:37:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = int(input("Введите число: "))
+x = int(input("Введите число: ")) #ввод числа
 
-for i in range (0,x+1):
+for i in range (0,x+1): # вывод всех четных чисел
     if i%2 == 0:
         print(i)
